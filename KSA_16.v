28e6cb065f4a8f9d8c8f8c7f08d2f034105fbc2a
Richa Singh <97393274+richa-singh01@users.noreply.github.com>
2023-07-10 20:30:07 +0530

    KSA_16.v

--- KSA_16.v
+++ KSA_16.v
@@ -19,40 +19,73 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-module KoggeStoneAdder_16(
-  input [15:0] a,
-  input [15:0] b,
-  input cin,
-  output [15:0] sum,
-  output cout
-);
 
-  wire [15:0] p, g, c;
-  wire [15:0] pp, gg, cp;
-  
-  assign pp[0] = a[0] ^ b[0];
-  assign gg[0] = a[0] & b[0];
-  assign cp[0] = cin;
+module KSA_16bit(input [15:0] a, input [15:0] b,output [16:0] s);  //16 bit Kogge stone adder
+wire [127:0] w;
+wire [15:0] c;
+genvar i;
 
-  assign p[0] = pp[0];
-  assign g[0] = gg[0];
-  assign c[0] = cp[0];
-  
-  genvar i;
-  
+generate
+    for(i=0; i<16; i=i+1)
+    begin : Propagate_Generate  //Label
+		Peres p_ran(.A(a[i]),.B(b[i]),.C(0),.P(),.Q(w[2*i+1]),.R(w[2*i]));				 
+    end
+endgenerate
+
+green_cell g1(w[1],w[0],w[33],w[32]);
+genvar g;
   generate
-    for (i = 1; i < 16; i = i + 1) begin
-      assign pp[i] = a[i] ^ b[i] ^ g[i - 1];
-      assign gg[i] = (a[i] & b[i]) | ((a[i] ^ b[i]) & g[i - 1]);
-      assign cp[i] = (a[i] & b[i]) | ((a[i] ^ b[i]) & cp[i - 1]);
-      
-      assign p[i] = pp[i] ^ (g[i - 1] & cp[i - 1]);
-      assign g[i] = gg[i] & cp[i - 1];
-      assign c[i] = gg[i] | (p[i] & cp[i - 1]);
+    for(g=0; g<15; g=g+1)
+    begin 
+		yellow_cell y_ran (.a(w[2*g+3]),.b(w[2*g+2]),.c(w[2*g+1]),.d(w[2*g]),.e(w[2*g+35]),.f(w[2*g+34]));	
     end
   endgenerate
-  
-  assign sum = {p[15], p[14], p[13], p[12], p[11], p[10], p[9], p[8], p[7], p[6], p[5], p[4], p[3], p[2], p[1], p[0]};
-  assign cout = c[15];
 
+green_cell g2(w[33],w[32],w[65] ,w[64]);
+green_cell g3(w[35],w[34],w[67],w[66]);
+  generate
+    for(i=0; i<14; i=i+1)
+    begin
+		yellow_cell y_ran1(.a(w[2*i+37]),.b(w[2*i+36]),.c(w[2*i+33]),.d(w[2*i+32]),.e(w[2*i+69]),.f(w[2*i+68]));				 
+    end
+  endgenerate
+  generate
+    for(i=0; i<4; i=i+1)
+    begin
+		green_cell g_ran(.a(w[2*i+65]),.b(w[2*i+64]),.c(w[2*i+97]),.d(w[2*i+96]));				 
+    end
+endgenerate
+generate
+    for(i=0; i<12; i=i+1)
+    begin
+		yellow_cell y_ran2(.a(w[2*i+73]),.b(w[2*i+72]),.c(w[2*i+65]),.d(w[2*i+64]),.e(w[2*i+105]),.f(w[2*i+104]));				 
+    end
+endgenerate
+
+
+
+
+
+generate
+    for(i=0; i<8; i=i+1)
+    begin 
+		green_cell g_ran1(.a(w[2*i+97]),.b(w[2*i+96]),.c(),.d(c[i]));				 
+    end
+endgenerate
+generate
+    for(i=0; i<8; i=i+1)
+    begin 
+	 yellow_cell yran (.a(w[2*i+113]),.b(w[2*i+112]),.c(w[2*i+97]),.d(w[2*i+96]),.e(),.f(c[i+8]));				 
+    end
+endgenerate
+assign s[16] = c[15];
+
+
+CNOT c1(w[1],0, ,s[0]);  
+generate
+    for(i=0; i<15; i=i+1)
+    begin
+		CNOT c_ran(.A(w[2*i+3]),.B(c[i]),.P(),.Q(s[i+1]));				 
+    end
+endgenerate 
 endmodule
