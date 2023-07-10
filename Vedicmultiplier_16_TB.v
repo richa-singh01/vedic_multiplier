abd6c8c98d8036bb3336ecbc8156c0708979dc35
Richa Singh <97393274+richa-singh01@users.noreply.github.com>
2023-07-10 20:23:43 +0530

    Update Vedicmultiplier_16_TB.v

--- Vedicmultiplier_16_TB.v
+++ Vedicmultiplier_16_TB.v
@@ -19,36 +19,36 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-module VedicMultiplier16_TB();
+module vedic16_tb;
 
-  reg [15:0] a;
-  reg [15:0] b;
-  wire [31:0] product;
+	// Inputs
+	reg [15:0] a;
+	reg [15:0] b;
 
-  // Instantiate the Vedic Multiplier module
-  VedicMultiplier_16x16 dut (
-    .multiplicand(a),
-    .multiplier(b),
-    .product(product)
-  );
+	// Outputs
+	wire [31:0] q;
 
-  initial begin
-    // Initialize input values
-    a = 16'h1234;
-    b = 16'h5678;
+	// Instantiate the Unit Under Test (UUT)
+	Vedic_16bit uut (
+		.a(a), 
+		.b(b), 
+		.q(q)
+	);
 
-    // Wait for some time to allow for computation
-    #10;
+	initial begin
+		// Initialize Inputs
+		a = 0;
+		b = 0;
 
-    // Display the input values
-    $display("Input a: %h", a);
-    $display("Input b: %h", b);
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		a= 16'b1010111101011101; b= 16'b0011001110101011; #250;
+		a= 16'b0110100101011101; b= 16'b1000111100001011; #250;
+		a= 16'b0101110110000100; b= 16'b1101111010000000; #250;
+		a= 16'b0001010001110001; b= 16'b1111110111111111; #250;
+		a= 16'b1100011011010100; b= 16'b0011000110011000; #250;
 
-    // Display the output product
-    $display("Product: %h", product);
-
-    // Finish simulation
-    $finish;
-  end
-
+	end
+      
 endmodule
